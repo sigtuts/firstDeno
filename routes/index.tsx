746252fb5101147566bf7cd712c3215a48446be9
Sigve Jordheim <sigve@Sigves-MacBook-Air.local>
2023-01-21 20:57:54 +0100

    index changes

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,28 +1,82 @@
 import { Head } from "$fresh/runtime.ts";
 import Counter from "../islands/Counter.tsx";
 
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Fresh App</title>
       </Head>
-      <div>
-        <img
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p>
-          Welcome to `fresh`, y'all. Try updating this message in the
-          ./routes/index.tsx file, and refresh.
-        </p>
-        <h2>
-          And remember, keep cool.
-        </h2>
-        <Counter start={0} />
+      <div style="    display: grid;
+     display: grid;
+     justify-content: space-around;
+     align-content: center;
+   
+     gap: 4px;
+     grid-auto-flow: column;">
+        <section style="    grid-column: 1;">
+          <img
+            src="/logo.svg"
+            width="128"
+            height="128"
+            alt="the fresh logo: a sliced lemon dripping with juice"
+          />
+          <p>
+            Welcome to `fresh`, y'all.
+          </p>
+          <h2>
+            And remember, keep cool.
+          </h2>
+          <Counter start={0} />
+        </section>
+        <section style="    grid-column: 2;">
+          <img
+            src="/logo.svg"
+            width="128"
+            height="128"
+            alt="the fresh logo: a sliced lemon dripping with juice"
+          />
+          <p>
+            Welcome to `fresh`, y'all.
+          </p>
+          <h2>
+            And remember, keep cool.
+          </h2>
+          <Counter start={0} />
+        </section>
+        <section style="    grid-column: 2;">
+          <img
+            src="/logo.svg"
+            width="128"
+            height="128"
+            alt="the fresh logo: a sliced lemon dripping with juice"
+          />
+          <p>
+            Welcome to `fresh`, y'all.
+          </p>
+          <h2>
+            And remember, keep cool.
+          </h2>
+          <Counter start={0} />
+        </section>
+        <section style="    grid-column: 3;">
+          <img
+            src="/logo.svg"
+            width="128"
+            height="128"
+            alt="the fresh logo: a sliced lemon dripping with juice"
+          />
+          <p>
+            Welcome to `fresh`, y'all.
+          </p>
+          <h2>
+            And remember, keep cool.
+          </h2>
+          <Counter start={0} />
+        </section>
       </div>
+
     </>
   );
 }
