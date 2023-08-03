77c62d5413fc8ee38ab7ee9af354dda6452249df
sigtuts <59766291+sigtuts@users.noreply.github.com>
2023-08-03 14:39:43 +0800

    1 file modified

--- islands/Geo.tsx
+++ islands/Geo.tsx
@@ -1,16 +1,17 @@
  export default function Geo() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
+    navigator.geolocation.getCurrentPosition(function go (position) {
       var latitude = position.coords.latitude;
       var longitude = position.coords.longitude;
       console.log('Latitude: ' + latitude + ', Longitude: ' + longitude);
+      return latitude
     });
   } else {
     console.log('Geolocation is not supported by this browser.');
   }
   return(
     <div>
-      <p>You are at LAT</p>
+      <p>You are at LAT{go()}</p>
     </div>
   )
 }
