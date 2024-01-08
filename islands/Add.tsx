ceb54cd1e35faf5a999d7957021043a45b9d5965
sigtuts <59766291+sigtuts@users.noreply.github.com>
2024-01-08 20:05:52 +0000

    Update Add.tsx

--- islands/Add.tsx
+++ islands/Add.tsx
@@ -4,7 +4,6 @@ export default function Add() {
 
     return (
       <div>
-        <p>{count}</p>
         {/* <Button onClick={() => setCount(count - 1)}>-1</Button> */}
         <input id="fir" type="number" />
         <input id="sec" type="number" />
