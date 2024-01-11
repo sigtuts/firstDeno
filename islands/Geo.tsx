ddd113eb9ffbcb34b506e0b547ca25cc59345b45
sigtuts <59766291+sigtuts@users.noreply.github.com>
2024-01-11 05:41:28 +0000

    Update Geo.tsx

--- islands/Geo.tsx
+++ islands/Geo.tsx
@@ -1,14 +1,29 @@
+// import { useState } from "preact/hooks";
+
+// export default function Geo() {
+// 	const [value, setValue] = useState(0);
+// 	const [value2, setValue2] = useState(0);
+// 	const num23 = parseInt(value) + parseInt(value2);
+
+
+// 	return (
+// 			<div>Counter: {num23}</div>
+// 			<input type="number" onInput={(e) => setValue(e.target.value)}></input>
+// 	    <input type="number" onInput={(e) => setValue2(e.target.value)}></input>
+// 	);
+// }
 import { useState } from "preact/hooks";
 
 export default function Geo() {
-	const [value, setValue] = useState(0);
-	const [value2, setValue2] = useState(0);
-	const num23 = parseInt(value) + parseInt(value2);
+  const [value, setValue] = useState(0);
+  const [value2, setValue2] = useState(0);
+  const num23 = parseInt(value) + parseInt(value2);
 
-
-	return (
-			<div>Counter: {num23}</div>
-			<input type="number" onInput={(e) => setValue(e.target.value)}></input>
-	    <input type="number" onInput={(e) => setValue2(e.target.value)}></input>
-	);
+  return (
+    <div>
+      <div>Counter: {num23}</div>
+      <input type="number" onInput={(e) => setValue(e.target.value)} />
+      <input type="number" onInput={(e) => setValue2(e.target.value)} />
+    </div>
+  );
 }
