9a9e6bc15e55f1d4d1d577d641ea096d77988c1e
sigtuts <59766291+sigtuts@users.noreply.github.com>
2024-01-11 05:43:07 +0000

    Update Geo.tsx

--- islands/Geo.tsx
+++ islands/Geo.tsx
@@ -1,17 +1,4 @@
-// import { useState } from "preact/hooks";
 
-// export default function Geo() {
-// 	const [value, setValue] = useState(0);
-// 	const [value2, setValue2] = useState(0);
-// 	const num23 = parseInt(value) + parseInt(value2);
-
-
-// 	return (
-// 			<div>Counter: {num23}</div>
-// 			<input type="number" onInput={(e) => setValue(e.target.value)}></input>
-// 	    <input type="number" onInput={(e) => setValue2(e.target.value)}></input>
-// 	);
-// }
 import { useState } from "preact/hooks";
 
 export default function Geo() {
@@ -22,6 +9,8 @@ export default function Geo() {
   return (
     <div>
       <div>Counter: {num23}</div>
+      <div>Counter: {value}</div>
+      <div>Counter: {value2}</div>
       <input type="number" onInput={(e) => setValue(e.target.value)} />
       <input type="number" onInput={(e) => setValue2(e.target.value)} />
     </div>
